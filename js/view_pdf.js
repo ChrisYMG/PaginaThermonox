bef0fa755fedea9225abc19fa5024edafaaedb6f
OmarLopGem <101218351+OmarLopGem@users.noreply.github.com>
2022-04-30 20:17:54 -0500

    Delete js directory

--- js/view_pdf.js
+++
@@ -1,8 +0,0 @@
-var button_view_pdf = document.getElementById("view_pdf").addEventListener("click", function(){ 
-    document.getElementById("iframe_pdf").classList.toggle("ver")
-}, false)
-
-var view_privacidad = document.getElementById("view_pdfp").addEventListener("click", function(){ 
-    document.getElementById("iframe_pdfp").classList.toggle("ver")
-}, false)
-
--- js/view_pdf_aviso_privacidad.js
+++
@@ -1,3 +0,0 @@
-var view_privacidad = document.getElementById("view_pdfp").addEventListener("click", function(){ 
-    document.getElementById("iframe_pdfp").classList.toggle("ver")
-}, false)
